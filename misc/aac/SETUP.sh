186b4c58dfc3dca14c334cf39e578c8d3b5878b0
Hisaki Ohara <Hisaki.Ohara@amd.com>
2025-08-09 16:02:16 +0000

    misc

---
+++ misc/aac/SETUP.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install -y vim
+sudo apt install -y iputils-ping
+sudo apt install -y iproute2
+sudo apt install -y tmux
+sudo apt install -y rdma-core
+sudo apt install -y hwloc
+sudo apt install -y git
+sudo apt install -y automake autoconf libtool pkg-config
+sudo apt install -y libibverbs-dev librdmacm-dev libibumad-dev libpci-dev
+sudo apt install python3.12-venv
